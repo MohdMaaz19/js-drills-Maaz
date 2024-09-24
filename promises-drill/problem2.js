671fd007541fc26f34bc4d4e184b4100f184f17c
mohdmaaz <mohdmaazshk@gmail.com>
2024-09-24 11:41:41 +0530

    Comments added to the file

--- promises-drill/problem2.js
+++ promises-drill/problem2.js
@@ -4,11 +4,11 @@ import path from "path";
 
 function fileModification(inputFilePath){
 
-    // Step 1: Read the given file `lipsum.txt`
+    // Read the given file `lipsum.txt`
     fs.readFile(inputFilePath, 'utf8')
 
     .then((content) => {
-    // Step 2: Convert the content to uppercase and write it to a new file
+    // Convert the content to uppercase and write it to a new file
     const upperContent = content.toUpperCase();
     const upperFile = 'uppercase_lipsum.txt';
     
@@ -18,7 +18,7 @@ function fileModification(inputFilePath){
     })
 
     .then((upperContent) => {
-    // Step 3: Convert the uppercase content to lowercase, split into sentences, and write to a new file
+    // Convert the uppercase content to lowercase, split into sentences, and write to a new file
     const lowerContent = upperContent.toLowerCase();
     const sentences = lowerContent.split('.').filter(Boolean).join('.\n');
     const lowerFile = 'lowercase_sentences.txt';
@@ -28,7 +28,7 @@ function fileModification(inputFilePath){
     })
 
     .then(() => {
-    // Step 4: Read both new files, sort the content, and write it to another file
+    // Read both new files, sort the content, and write it to another file
     return Promise.all([
         fs.readFile('uppercase_lipsum.txt', 'utf8'),
         fs.readFile('lowercase_sentences.txt', 'utf8')
@@ -45,7 +45,7 @@ function fileModification(inputFilePath){
     })
 
     .then(() => {
-    // Step 5: Read `filenames.txt` and delete all files listed in it
+    // Read `filenames.txt` and delete all files listed in it
     return fs.readFile('./output/filenames.txt', 'utf8');
     })
 
